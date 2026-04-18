9845b8a8f62b8289c1aee38c7828a69a4c55d8c8
jihan-hanhant <jihan2111@outlook.com>
2026-04-18 11:01:16 +0800

    a

--- assets/textures/test_hex_u-rotation.gd
+++ assets/textures/test_hex_u-rotation.gd
@@ -1,5 +1,6 @@
-extends Node
+extends Node3D
 
+@export var RotateSpeed : float 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,4 +9,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	rotate_y(RotateSpeed * delta)
