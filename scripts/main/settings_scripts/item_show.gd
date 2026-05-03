cfbb416ce137129f94e96e7160f2e4f95a9beab6
jihan-hanhant <jihan2111@outlook.com>
2026-05-03 22:42:08 +0800

    完成设置界面基本

---
+++ scripts/main/settings_scripts/item_show.gd
@@ -0,0 +1,7 @@
+extends Control
+
+func _on_toggler_show() -> void:
+	self.visible = true
+
+func _on_toggler_hide() -> void:
+	self.visible = false
--- scripts/main/settings_scripts/toggle_controler.gd
+++ scripts/main/settings_scripts/toggle_controler.gd
@@ -1,11 +1,19 @@
 extends Node
 
+var latest_num:int = 0
+var latest_weight:Node
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func toggle(num:int,path_weight:NodePath) -> void:
+	var weight = get_node(path_weight)
+	
+	if latest_num == 0:
+		weight._on_toggler_show()
+	elif latest_num == num:
+		weight._on_toggler_show()
+	elif not latest_num == num:
+		latest_weight._on_toggler_hide()
+		weight._on_toggler_show()
+	
+	latest_num = num
+	latest_weight = weight
+	
