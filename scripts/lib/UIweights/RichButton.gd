6816d7c8a4b1d1c631f0f9653b406d045df919ca
jihan-hanhant <jihan2111@outlook.com>
2026-05-16 20:25:56 +0800

    小变化

---
+++ scripts/lib/UIweights/RichButton.gd
@@ -0,0 +1,35 @@
+@tool
+extends Button
+class_name RichButton
+
+@export var normal_color : Color
+@export var corner_radius : int = 5
+
+func _ready() -> void:
+	binit("normal",normal_color)
+	binit("hover",normal_color.lerp(Color.WHITE,0.2))
+	binit("pressed",normal_color.lerp(Color.BLACK,0.1))
+
+#本体初始化	
+func binit(style_name:String,setcolor:Color):
+	var origin_style = self.get_theme_stylebox(style_name)
+	var style:StyleBoxFlat = origin_style.duplicate()
+	style.bg_color = normal_color
+	
+	var border_size : int = (self.size.y / 200) * 8
+	style.border_width_left = border_size
+	style.border_width_top = border_size
+	style.border_width_right = border_size
+	style.border_width_bottom = border_size
+	
+	style.border_color = setcolor.lerp(Color.WHITE,0.4)
+	style.corner_radius_top_left = corner_radius
+	style.corner_radius_top_right = corner_radius
+	style.corner_radius_bottom_left = corner_radius
+	style.corner_radius_bottom_right = corner_radius
+	style.corner_detail = int(self.size.y) / 10
+	
+	self.add_theme_stylebox_override(style_name,style)
+
+func tinit() -> void:
+	var t = Label.new()
